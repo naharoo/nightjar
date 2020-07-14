0cf2e67ffe1fbfa6698c62bdfc3afec340b6f8c7
naharoo <narek.harutyunyan@sfl.am>
2020-07-14 16:15:41 +0400

    Some styling changes.

--- src/main/resources/static/js/custom/editor/editor.js
+++ src/main/resources/static/js/custom/editor/editor.js
@@ -116,7 +116,22 @@ function drawQualifiers() {
             if (isNewSnippet) {
                 window.snippetDetails.qualifiersMultiselect = new SelectPure(".qualifiers", {
                     options: multiselectOptions,
-                    multiple: true
+                    value: [],
+                    multiple: true,
+                    classNames: {
+                        select: "select-pure__select",
+                        dropdownShown: "select-pure__select--opened",
+                        multiselect: "select-pure__select--multiple",
+                        label: "select-pure__label",
+                        placeholder: "select-pure__placeholder",
+                        dropdown: "select-pure__options",
+                        option: "select-pure__option",
+                        autocompleteInput: "select-pure__autocomplete",
+                        selectedLabel: "select-pure__selected-label",
+                        selectedOption: "select-pure__option--selected",
+                        placeholderHidden: "select-pure__placeholder--hidden",
+                        optionHidden: "select-pure__option--hidden",
+                    }
                 });
             } else {
                 const snippetId = window.snippet.id;
@@ -128,7 +143,21 @@ function drawQualifiers() {
                         window.snippetDetails.qualifiersMultiselect = new SelectPure(".qualifiers", {
                             options: multiselectOptions,
                             value: snippet.qualifiers,
-                            multiple: true
+                            multiple: true,
+                            classNames: {
+                                select: "select-pure__select",
+                                dropdownShown: "select-pure__select--opened",
+                                multiselect: "select-pure__select--multiple",
+                                label: "select-pure__label",
+                                placeholder: "select-pure__placeholder",
+                                dropdown: "select-pure__options",
+                                option: "select-pure__option",
+                                autocompleteInput: "select-pure__autocomplete",
+                                selectedLabel: "select-pure__selected-label",
+                                selectedOption: "select-pure__option--selected",
+                                placeholderHidden: "select-pure__placeholder--hidden",
+                                optionHidden: "select-pure__option--hidden",
+                            }
                         });
                     },
                     error: showErrorPopup
